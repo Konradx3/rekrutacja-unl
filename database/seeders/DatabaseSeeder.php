82f41eff277198bbfa1c3de720986d2d4fe508bf
Konrad <konasgpro@gmail.com>
2024-08-13 22:15:09 +0200

    feat: ApiResponse layer, login and logout user, users migration update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Admin user
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Employee user
+        User::create([
+            'name' => 'Employee',
+            'email' => 'employee@example.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/hello', function () {
     return 'Hello API!';
 });
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
